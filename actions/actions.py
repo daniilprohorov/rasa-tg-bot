2c9c8a23effaecd37487e0e14ebcef6aa5d3f7ed
daniilprohorov <daniilprohorov@gmail.com>
2024-01-03 23:23:53 +0300

    form works

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,27 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+class ActionChangeSlotSetNull(Action):
+    def name(self) -> Text:
+        return "action_change_slot_set_null"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("change_slot", "not_set")]
+
+class ActionRegisterFormSlotsReset(Action):
+    def name(self) -> Text:
+        return "action_register_form_slots_reset"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("engine_slot", None), SlotSet("transmission_slot", None), SlotSet("model_slot", None)]
